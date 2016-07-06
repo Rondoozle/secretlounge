b0fa0cbe6593b359ef9e9761b76cafd223f3b225
Daniel Bugl <me@omnidan.net>
2016-07-05 20:45:15 -0400

    show join and part messages

--- src/index.js
+++ src/index.js
@@ -11,16 +11,24 @@ const networks = connect(config)
 
 import { getUser, addUser, delUser, getUsers } from './db'
 
+const sendToAll = (rawEvent) => {
+  let evt
+  if (typeof rawEvent === 'string') evt = { type: 'message', text: rawEvent }
+  else evt = rawEvent
+
+  getUsers().map((user) => {
+    if (config.debug || user.id !== evt.user) { // don't relay back to sender
+      networks.send({ ...evt, chat: user.id })
+    }
+  })
+}
+
 const relay = (type) => {
   networks.on(type, (evt, reply) => {
     if (evt.text.charAt(0) !== '/') { // don't parse commands again
       if (getUser(evt.user)) { // make sure user is in the group chat
-        // otherwise, relay message to all users
-        getUsers().map((user) => {
-          if (config.debug || user.id !== evt.user) { // don't relay back to sender
-            networks.send({ ...evt, chat: user.id })
-          }
-        })
+        // otherwise, relay event to all users
+        sendToAll(evt)
       } else {
         reply('You\'re not chatting in /b/ yet! Use /start')
       }
@@ -30,13 +38,15 @@ const relay = (type) => {
 
 ['message', 'audio', 'document', 'photo', 'sticker', 'video', 'voice'].map(relay)
 
-const getUsername = (user) => user.username
+const getUsername = (user) => '@' + user.username
+const getUsernameFromEvent = (evt) => evt.raw && evt.raw.from && evt.raw.from.username
 
 const commands = (cmd, evt, reply) => {
   switch (cmd) {
     case 'stop':
       if (!getUser(evt.user)) reply('You\'re not chatting in /b/ yet! Use /start')
       else delUser(evt.user)
+      sendToAll('@' + getUsernameFromEvent(evt) + ' just left /b/!')
       break
     case 'users':
       const users = getUsers()
@@ -53,7 +63,9 @@ networks.on('command', (evt, reply) => {
   if (cmd === 'start') {
     if (getUser(evt.user)) return reply('You\'re already chatting in /b/!')
     else {
-      addUser(evt.user, evt.raw && evt.raw.from && evt.raw.from.username)
+      const username = getUsernameFromEvent(evt)
+      addUser(evt.user, username)
+      sendToAll('@' + username + ' just joined /b/!')
       return reply('Welcome to /b/!')
     }
   } else {
