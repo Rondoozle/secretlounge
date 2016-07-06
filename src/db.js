ebb881b0c49edcbaa9b0e691546d2c059c67d78e
Daniel Bugl <me@omnidan.net>
2016-07-05 20:30:54 -0400

    only let joined users talk, add /users command

--- src/db.js
+++ src/db.js
@@ -4,6 +4,6 @@ const db = low('db.json')
 db.defaults({ users: [] }).value()
 
 export const getUser = (id) => db.get('users').find({ id }).value()
-export const addUser = (id) => db.get('users').push({ id }).value()
+export const addUser = (id, username) => db.get('users').push({ id, username }).value()
 export const delUser = (id) => db.get('users').remove({ id }).value()
 export const getUsers = () => db.get('users').value()
--- src/index.js
+++ src/index.js
@@ -13,39 +13,51 @@ import { getUser, addUser, delUser, getUsers } from './db'
 
 const relay = (type) => {
   networks.on(type, (evt, reply) => {
-    if (evt.text.charAt(0) !== '/') { // don't relay commands
-      // relay message to all users
-      getUsers().map((user) => {
-        if (config.debug || user.id !== evt.user) { // don't relay back to sender
-          networks.send({ ...evt, chat: user.id })
-        }
-      })
+    if (evt.text.charAt(0) !== '/') { // don't parse commands again
+      if (getUser(evt.user)) { // make sure user is in the group chat
+        // otherwise, relay message to all users
+        getUsers().map((user) => {
+          if (config.debug || user.id !== evt.user) { // don't relay back to sender
+            networks.send({ ...evt, chat: user.id })
+          }
+        })
+      } else {
+        reply('You\'re not chatting in /b/ yet! Use /start')
+      }
     }
   })
 }
 
 ['message', 'audio', 'document', 'photo', 'sticker', 'video', 'voice'].map(relay)
 
-const commands = (cmd, evt) => {
+const getUsername = (user) => user.username
+
+const commands = (cmd, evt, reply) => {
   switch (cmd) {
     case 'stop':
       if (!getUser(evt.user)) reply('You\'re not chatting in /b/ yet! Use /start')
       else delUser(evt.user)
       break
+    case 'users':
+      const users = getUsers()
+      reply(users.length + ' users: ' + users.map(getUsername).join(', '))
+      break
   }
 }
 
 networks.on('command', (evt, reply) => {
+  log('Received command event: %o', evt)
+
   const cmd = evt.cmd.toLowerCase()
 
   if (cmd === 'start') {
     if (getUser(evt.user)) return reply('You\'re already chatting in /b/!')
     else {
-      addUser(evt.user)
+      addUser(evt.user, evt.raw && evt.raw.from && evt.raw.from.username)
       return reply('Welcome to /b/!')
     }
   } else {
     if (!getUser(evt.user)) return reply('You\'re not chatting in /b/ yet! Use /start')
-    commands(cmd, evt)
+    commands(cmd, evt, reply)
   }
 })
