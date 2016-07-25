99952aa1a1709fd2eb8c54485cbea83375dd1dc0
Daniel Bugl <me@omnidan.net>
2016-07-25 19:32:32 -0400

    don't show version message after joining

--- src/db.js
+++ src/db.js
@@ -1,11 +1,13 @@
 import low from 'lowdb'
 const db = low('db.json')
 
+import { version } from '../package.json'
+
 db.defaults({ users: [], system: {} }).value()
 
 export const getUser = (id) => db.get('users').find({ id }).value()
 export const getUserByUsername = (username) => db.get('users').find({ username }).value()
-export const addUser = (id) => db.get('users').push({ id, rank: 0 }).value()
+export const addUser = (id) => db.get('users').push({ id, rank: 0, version }).value()
 export const rejoinUser = (id) => db.get('users').find({ id }).assign({ kicked: false, banned: false }).value()
 export const delUser = (id) => db.get('users').remove({ id }).value()
 export const getUsers = () => db.get('users').value()
